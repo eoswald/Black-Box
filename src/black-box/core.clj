d286c5ab88003cec61ab5b9744c187b9cca2dea9
Eric Oswald <eoswald39@gmail.com>
2014-10-16 17:19:19 -0400

    Renamed folder in source to get things to run and played with ring.

--- src/black-box/core.clj
+++
@@ -1,6 +0,0 @@
-(ns black-box.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
---
+++ src/black_box/core.clj
@@ -0,0 +1,6 @@
+(ns black-box.core)
+
+(defn handler [request]
+	{:status 200
+	 :headers {"Content-Type" "text/html"}
+	 :body "Hello World"})
